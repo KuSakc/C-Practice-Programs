cef9fda6c44595fe98429d90b8c2774ecae305c4
KuSakc <sakshamparajuli.official@gmail.com>
2026-07-09 15:24:41 +0545

    Added "check_palindrome.c" with Code

---
+++ Basic/check_palindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int number;
+
+    printf("Number: ");scanf("%d", &number);
+
+        int reverse=0,temp;
+    for (int i=1;   ; number/10)
+    {
+        reverse = reverse *10 + (number -(number/10)*10)
+        
+    }
+}
